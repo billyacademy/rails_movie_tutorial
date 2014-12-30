8904f45b7e59e14e309dc6a1608aad67370cfcb8
William Mahoney <wm.j.mahoney@gmail.com>
2014-12-29 21:44:21 -0500

    add paperclip for image upload

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,6 @@
 class MoviesController < ApplicationController
   before_action :set_movie, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_user!, except: [:index, :show]
   def index
     @movies = Movie.all
   end
@@ -58,6 +58,6 @@ class MoviesController < ApplicationController
     end
 
     def movie_params
-      params.require(:movie).permit(:title, :description, :movie_length, :director, :rating)
+      params.require(:movie).permit(:title, :description, :movie_length, :director, :rating, :image)
     end
 end
